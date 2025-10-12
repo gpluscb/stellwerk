64e9014e86b7756fd862710836b670e265a6d7f9
MarRue <20143778+gpluscb@users.noreply.github.com>
2025-10-12 17:30:14 +0200

    Improve init error handling

--- socialmediathingnametbd-api/src/main.rs
+++ socialmediathingnametbd-api/src/main.rs
@@ -1,9 +1,22 @@
 use axum::routing::get;
 use serde::Deserialize;
 use std::net::{IpAddr, SocketAddr};
+use thiserror::Error;
 use tower_http::trace::TraceLayer;
 use tracing::debug;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[derive(Debug, Error)]
+enum InitError {
+    #[error("Error parsing .env file: {0}")]
+    Dotenv(#[from] dotenvy::Error),
+    #[error("Error parsing environment: {0}")]
+    Envy(#[from] envy::Error),
+    #[error("Error binding tcp listener: {0}")]
+    TcpBind(std::io::Error),
+    #[error("Error serving server: {0}")]
+    TcpServe(std::io::Error),
+}
 
 #[derive(Clone, Eq, PartialEq, Debug, Hash, Deserialize)]
 struct Env {
@@ -25,22 +38,22 @@ fn install_tracing() {
         .init();
 }
 
-fn get_env() -> Env {
+fn get_env() -> Result<Env, InitError> {
     if let Err(e) = dotenvy::dotenv() {
         if e.not_found() {
             debug!("No .dotenv file found");
         } else {
-            panic!("Error parsing .env file: {e}");
+            return Err(e.into());
         }
     }
 
-    envy::from_env().expect("Environment could not be parsed")
+    envy::from_env().map_err(InitError::from)
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), InitError> {
     install_tracing();
-    let env = get_env();
+    let env = get_env()?;
 
     let tracing_layer = TraceLayer::new_for_http();
     let app = axum::Router::new()
@@ -50,6 +63,10 @@ async fn main() {
     let server_address = SocketAddr::new(env.server_address, env.server_port);
     let listener = tokio::net::TcpListener::bind(server_address)
         .await
-        .expect("Could not bind");
-    axum::serve(listener, app).await.expect("Could not serve");
+        .map_err(InitError::TcpBind)?;
+    axum::serve(listener, app)
+        .await
+        .map_err(InitError::TcpServe)?;
+
+    Ok(())
 }
