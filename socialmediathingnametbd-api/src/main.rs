8ebd49f75a8d74b145075a083f08633574e80644
MarRue <20143778+gpluscb@users.noreply.github.com>
2025-10-12 12:19:54 +0200

    first working api server

--- socialmediathingnametbd-api/src/main.rs
+++ socialmediathingnametbd-api/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use axum::routing::get;
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                "socialmediathingnametbd_api=debug,\
+                socialmediathingnametbd_common=debug,\
+                tower_http=debug,axum::rejection=trace,sqlx=debug"
+                    .into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let tracing_layer = TraceLayer::new_for_http();
+    let app = axum::Router::new()
+        .route("/", get(|| async { "hi" }))
+        .layer(tracing_layer);
+
+    let listener = tokio::net::TcpListener::bind("localhost:8080")
+        .await
+        .unwrap();
+    axum::serve(listener, app).await.expect("Couldn't serve");
 }
