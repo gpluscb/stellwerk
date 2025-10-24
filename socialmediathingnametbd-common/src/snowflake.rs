133d256a9f89fcafbb240b4a61bcb9d54002c92c
gpluscb <20143778+gpluscb@users.noreply.github.com>
2025-10-24 12:59:36 +0200

    Unit tests for snowflake.rs

--- socialmediathingnametbd-common/src/snowflake.rs
+++ socialmediathingnametbd-common/src/snowflake.rs
@@ -12,7 +12,7 @@ use std::{
     marker::PhantomData,
 };
 use thiserror::Error;
-use time::UtcDateTime;
+use time::{Duration, UtcDateTime};
 
 #[allow(clippy::unusual_byte_groupings)]
 pub const TIMESTAMP_BITMASK: u64 =
@@ -38,7 +38,7 @@ pub const INCREMENT_BITMASK: u64 =
 pub const INCREMENT_OFFSET: u64 = 0;
 pub const INCREMENT_LENGTH: u64 = 12;
 
-#[derive(Clone, Debug, Error)]
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Debug, Hash, Error)]
 pub enum SnowflakeTimestampFromDateTimeError {
     #[error("Specified time was before the snowflake epoch.")]
     TimeBeforeEpoch,
@@ -195,6 +195,13 @@ impl<SnowflakeEpoch: Epoch> TryFrom<UtcDateTime> for SnowflakeTimestamp<Snowflak
     }
 }
 
+impl<SnowflakeEpoch: Epoch> From<SnowflakeTimestamp<SnowflakeEpoch>> for UtcDateTime {
+    fn from(value: SnowflakeTimestamp<SnowflakeEpoch>) -> Self {
+        SnowflakeEpoch::EPOCH_TIME
+            + Duration::milliseconds(value.0.try_into().expect("Invalid timestamp value"))
+    }
+}
+
 impl<SnowflakeEpoch> Snowflake<SnowflakeEpoch> {
     #[must_use]
     pub fn new(inner: u64) -> Self {
@@ -309,7 +316,7 @@ impl<SnowflakeEpoch> SnowflakeGenerator<SnowflakeEpoch> {
         self.process_id
     }
 
-    pub fn generate(&mut self) -> Snowflake<SnowflakeEpoch>
+    pub fn generate_at(&mut self, time: UtcDateTime) -> Snowflake<SnowflakeEpoch>
     where
         SnowflakeEpoch: Epoch,
     {
@@ -317,10 +324,171 @@ impl<SnowflakeEpoch> SnowflakeGenerator<SnowflakeEpoch> {
         self.next_increment.increment();
 
         Snowflake::from_parts(
-            SnowflakeTimestamp::now(),
+            SnowflakeTimestamp::from_time_unchecked(time),
             self.worker_id,
             self.process_id,
             increment,
         )
+    }
+
+    pub fn generate(&mut self) -> Snowflake<SnowflakeEpoch>
+    where
+        SnowflakeEpoch: Epoch,
+    {
+        self.generate_at(UtcDateTime::now())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::snowflake::{
+        Epoch, ProcessId, Snowflake, SnowflakeGenerator, SnowflakeIncrement, SnowflakeTimestamp,
+        SnowflakeTimestampFromDateTimeError, WorkerId,
+    };
+    use time::{Duration, UtcDateTime, macros::utc_datetime};
+
+    struct MillennialEpoch;
+    impl Epoch for MillennialEpoch {
+        const EPOCH_TIME: UtcDateTime = utc_datetime!(2000-1-1 00:00);
+    }
+
+    #[test]
+    fn legal_values() {
+        let legal_timestamps = [0, 0xFFFF, 0x03FF_FFFF_FFFF];
+        let illegal_timestamps = [0x0400_0000_0000, 0x08F0_0000_0000_0000, u64::MAX];
+
+        for legal_timestamp in legal_timestamps {
+            assert!(SnowflakeTimestamp::<MillennialEpoch>::new(legal_timestamp).is_some());
+        }
+        for illegal_timestamp in illegal_timestamps {
+            assert!(SnowflakeTimestamp::<MillennialEpoch>::new(illegal_timestamp).is_none());
+        }
+
+        let legal_ids = [0, 0xD, 0x1F];
+        let illegal_ids = [0x20, 0xF0, u8::MAX];
+
+        for legal_id in legal_ids {
+            assert!(WorkerId::new(legal_id).is_some());
+            assert!(ProcessId::new(legal_id).is_some());
+        }
+        for illegal_id in illegal_ids {
+            assert!(WorkerId::new(illegal_id).is_none());
+            assert!(ProcessId::new(illegal_id).is_none());
+        }
+
+        let legal_increments = [0, 0xFF, 0xFFF];
+        let illegal_increments = [0x1000, 0xFF00, u16::MAX];
+
+        for legal_increment in legal_increments {
+            assert!(SnowflakeIncrement::new(legal_increment).is_some());
+            assert!(SnowflakeIncrement::new(legal_increment).is_some());
+        }
+        for illegal_increment in illegal_increments {
+            assert!(SnowflakeIncrement::new(illegal_increment).is_none());
+            assert!(SnowflakeIncrement::new(illegal_increment).is_none());
+        }
+    }
+
+    #[test]
+    fn snowflake_timestamp() {
+        let legal_date_times = [
+            MillennialEpoch::EPOCH_TIME,
+            utc_datetime!(2025-10-24 10:00),
+            MillennialEpoch::EPOCH_TIME + Duration::milliseconds(0x03FF_FFFF_FFFF),
+        ];
+
+        for legal_date_time in legal_date_times {
+            let timestamp =
+                SnowflakeTimestamp::<MillennialEpoch>::try_from(legal_date_time).unwrap();
+            assert_eq!(UtcDateTime::from(timestamp), legal_date_time);
+        }
+
+        assert_eq!(
+            SnowflakeTimestamp::<MillennialEpoch>::try_from(
+                MillennialEpoch::EPOCH_TIME - Duration::milliseconds(1)
+            ),
+            Err(SnowflakeTimestampFromDateTimeError::TimeBeforeEpoch)
+        );
+
+        assert_eq!(
+            SnowflakeTimestamp::<MillennialEpoch>::try_from(
+                MillennialEpoch::EPOCH_TIME + Duration::milliseconds(0x0400_0000_0000)
+            ),
+            Err(SnowflakeTimestampFromDateTimeError::TimestampTooLarge)
+        );
+    }
+
+    #[test]
+    fn snowflake_increment() {
+        assert_eq!(
+            SnowflakeIncrement::new_unchecked(0).next(),
+            SnowflakeIncrement::new_unchecked(1)
+        );
+        assert_eq!(
+            SnowflakeIncrement::new_unchecked(100).next(),
+            SnowflakeIncrement::new_unchecked(101)
+        );
+        assert_eq!(
+            SnowflakeIncrement::new_unchecked(0xFFF).next(),
+            SnowflakeIncrement::new_unchecked(0)
+        );
+
+        let mut snowflake_increment = SnowflakeIncrement::new_unchecked(0xFFE);
+        snowflake_increment.increment();
+        assert_eq!(
+            snowflake_increment,
+            SnowflakeIncrement::new_unchecked(0xFFF)
+        );
+        snowflake_increment.increment();
+        assert_eq!(snowflake_increment, SnowflakeIncrement::new_unchecked(0));
+    }
+
+    #[test]
+    fn snowflake_from_into_parts() {
+        let timestamp = SnowflakeTimestamp::from_time_unchecked(utc_datetime!(2025-10-24 10:30));
+        let worker_id = WorkerId::new_unchecked(0b10101);
+        let process_id = ProcessId::new_unchecked(0b10001);
+        let increment = SnowflakeIncrement::new_unchecked(100);
+
+        let snowflake =
+            Snowflake::<MillennialEpoch>::from_parts(timestamp, worker_id, process_id, increment);
+
+        assert_eq!(snowflake.get(), 3_416_751_341_570_822_244);
+
+        assert_eq!(snowflake.timestamp(), timestamp);
+        assert_eq!(snowflake.worker_id(), worker_id);
+        assert_eq!(snowflake.process_id(), process_id);
+        assert_eq!(snowflake.increment(), increment);
+    }
+
+    #[test]
+    fn snowflake_generator() {
+        let worker_id = WorkerId::new_unchecked(10);
+        let process_id = ProcessId::new_unchecked(0);
+        let time = utc_datetime!(2025-10-24 10:55);
+
+        let mut generator = SnowflakeGenerator::<MillennialEpoch>::new(worker_id, process_id);
+
+        let first_snowflake = generator.generate_at(time);
+        assert_eq!(
+            first_snowflake,
+            Snowflake::from_parts(
+                SnowflakeTimestamp::from_time_unchecked(time),
+                worker_id,
+                process_id,
+                SnowflakeIncrement::new_unchecked(0)
+            )
+        );
+
+        let second_snowflake = generator.generate_at(time);
+        assert_eq!(
+            second_snowflake,
+            Snowflake::from_parts(
+                SnowflakeTimestamp::from_time_unchecked(time),
+                worker_id,
+                process_id,
+                SnowflakeIncrement::new_unchecked(1)
+            )
+        );
     }
 }
